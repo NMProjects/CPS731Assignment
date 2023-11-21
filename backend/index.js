2d7a33b2a270992342fa57a39b7b76327e4bbd3f
patcunko <71910223+patcunko@users.noreply.github.com>
2023-11-21 15:38:35 -0500

    Update index.js
    added skeleton code for connecting to oracle database

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
+const sql = require('mssql')
+
+// code for connecting to database
+// need to choose which user for ID and Password values
+// also run comman npm install mssql
+async () => {
+    try {
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect('Server=oracle.scs.ryerson.ca,1521;Database=database;User Id=username;Password=password;Encrypt=true')
+        const result = await sql.query`select * from mytable where id = ${value}`
+        console.dir(result)
+    } catch (err) {
+        // ... error checks
+    }
+}
+
 const express = require("express");
 const app = express();
 const port = 3000;
