62d2237a46d2dffd51ee27966ae0f0f42a03484e
Thorley <thorley.eric@gmail.com>
2023-11-25 15:30:08 -0500

    added php form handling and seperated javascript

---
+++ backend/course_review.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Difficulty <?php echo $_POST["courseDifficultyInputsNumber"]; ?><br>
+Enjoyment: <?php echo $_POST["courseEnjoymentInputsNumber"]; ?><br>
+Material: <?php echo $_POST["courseMaterialInputsNumber"]; ?><br>
+Review: <?php echo $_POST["courseReview"]; ?><br>
+
+
+</body>
+</html> 
---
+++ backend/radio_buttons.js
@@ -0,0 +1,20 @@
+function createRadioButtons(divID){
+
+    var radioInputsContainer = document.getElementById(divID);
+  
+    for (var i = 1; i <= 10; i++) {
+      var radioInput = document.createElement("input");
+      radioInput.type = "radio";
+      radioInput.id = "radio" + i;
+      radioInput.name = divID +"Number";
+      radioInput.value = i;
+  
+      var label = document.createElement("label");
+      label.htmlFor = "radio" + i;
+      label.appendChild(document.createTextNode(i));
+  
+      radioInputsContainer.appendChild(radioInput);
+      radioInputsContainer.appendChild(label);
+    }
+}
+  
