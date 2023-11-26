29571e254c34cf43eb751332a9c7feb635f0ffcf
Thorley <thorley.eric@gmail.com>
2023-11-26 16:36:56 -0500

    Register is live with my database

--- backend/register.php
+++ backend/register.php
@@ -1,12 +1,28 @@
-<html>
-<body>
-<?php echo $_POST["uname"]; ?><br>
+
 <?php 
-    $pword = $_POST["pword"];
-    $cryptpass = crypt($pword, PASSWORD_BCRYPT);
-    echo $cryptpass;
+$conn = oci_connect('ethorley', '17172113',
+'(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))');
+if ($conn){ 
+    print("SOMETHING");
+}else{
+    print("Connection Failed ");
+    $m = oci_error();
+    print($m['message']);
+  }
+$uname = filter_var($_POST["uname"], FILTER_SANITIZE_EMAIL);
+$pword = $_POST["pword"];
+$cryptpass = crypt($pword, PASSWORD_BCRYPT);
+
+echo $uname;
+
+$query = "INSERT INTO USERS (email, pword) VALUES ($uname, $cryptpass)";
+$stid = oci_parse($conn, $query);
+$r = oci_execute($stid);
+echo "$uname registered";
+print "<br>";
+
+oci_close($conn);
 
 ?>
 
-</body>
-</html>
+
