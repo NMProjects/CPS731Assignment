c4f7720d9098bc15a5253a607de1694f20ec2d38
Thorley <thorley.eric@gmail.com>
2023-11-26 18:08:38 -0500

    course review form updated

--- backend/course_review.php
+++ backend/course_review.php
@@ -1,11 +1,12 @@
 <html>
 <body>
 
+Course: <?php echo $_POST["courseCode"]; ?><br>
 Difficulty <?php echo $_POST["courseDifficultyInputsNumber"]; ?><br>
 Enjoyment: <?php echo $_POST["courseEnjoymentInputsNumber"]; ?><br>
 Material: <?php echo $_POST["courseMaterialInputsNumber"]; ?><br>
 Review: <?php echo $_POST["courseReview"]; ?><br>
-
+Grade: <?php echo $_POST["courseGrade"]; ?>
 
 </body>
 </html> 
--- backend/radio_buttons.js
+++ backend/radio_buttons.js
@@ -1,20 +1,27 @@
 function createRadioButtons(divID){
 
-    var radioInputsContainer = document.getElementById(divID);
-  
-    for (var i = 1; i <= 10; i++) {
-      var radioInput = document.createElement("input");
-      radioInput.type = "radio";
-      radioInput.id = "radio" + i;
-      radioInput.name = divID +"Number";
-      radioInput.value = i;
-  
-      var label = document.createElement("label");
-      label.htmlFor = "radio" + i;
-      label.appendChild(document.createTextNode(i));
-  
-      radioInputsContainer.appendChild(radioInput);
-      radioInputsContainer.appendChild(label);
-    }
+  var radioInputsContainer = document.getElementById(divID);
+
+  for (var i = 1; i <= 10; i++) {
+    var radioInput = document.createElement("input");
+    radioInput.type = "radio";
+    radioInput.id = "radio" + i;
+    radioInput.name = divID +"Number";
+    radioInput.value = i;
+
+    var label = document.createElement("label");
+    label.htmlFor = "radio" + i;
+    label.appendChild(document.createTextNode(i));
+
+    radioInputsContainer.appendChild(radioInput);
+    radioInputsContainer.appendChild(label);
+  }
+}
+function createGradeDropdown(divID){
+  var dropdown = document.getElementById(divID);
+  for (var i = 100; i >= 0; i--) {
+    o = new Option(i,i)
+    dropdown.appendChild(o);
+  }
 }
   
