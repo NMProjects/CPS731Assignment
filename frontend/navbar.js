18480f5c9797575eed028907eaa01d37babacdc6
nahid <nahid02m@gmail.com>
2023-12-02 18:17:22 -0500

    Removed combination folder, introduced nav bar to every page

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -2,27 +2,45 @@ var htmlPages = [
     "main",
     "course_review_form",
     "prof_review_form",
-    "course_review_form",
+    "university_review_form",
+    "view_course_review",
     "view_prof",
+    "view_uni_review",
     "register",
     "login",
     "logout",
-
-
 ];
 
-function createNavbar(){
-    var nav = document.getElementsByClassName("navbar")[0]
-    console.log(nav)
-    for (var i in htmlPages){
-        var li = document.createElement("li")
+var niceNames = [
+    "Home",
+    "Review a course",
+    "Review a prof",
+    "Review a university",
+    "View Courses",
+    "View Professors",
+    "View Universities",
+    "Register",
+    "Login",
+    "Logout",
+];
+async function createNavbar() {
+    var nav = document.getElementsByClassName("navbar")[0];
+    console.log(nav);
+    for (var i in htmlPages) {
+        var buttons = document.createElement("button");
         var name = htmlPages[i];
         var ref = name + ".html";
-        var tag = document.createElement("a");
-        tag.setAttribute("href",ref);
-        tag.innerHTML = name;
-        li.appendChild(tag);
-        nav.appendChild(li);
-    } 
-};
+        buttons.onclick = (function (ref) {
+            return function () {
+                goLink(ref);
+            };
+        })(ref);
 
+        buttons.innerText = niceNames[i];
+        await nav.appendChild(buttons);
+    }
+}
+
+function goLink(link) {
+    window.location.href = link;
+}
