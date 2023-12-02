9bc4f12bdb424170a5ac74c0bc3de42522d17d02
nahid <nahid02m@gmail.com>
2023-12-02 17:50:18 -0500

    Fixed combination folder

--- combination/navbar.js
+++ combination/navbar.js
@@ -8,18 +8,24 @@ var htmlPages = [
     "login",
     "logout",
 ];
-
-function createNavbar() {
+async function createNavbar() {
     var nav = document.getElementsByClassName("navbar")[0];
     console.log(nav);
     for (var i in htmlPages) {
         var buttons = document.createElement("button");
         var name = htmlPages[i];
         var ref = name + ".html";
-        var tag = document.createElement("a");
-        tag.setAttribute("href", ref);
-        tag.innerHTML = name;
-        buttons.appendChild(tag);
-        nav.appendChild(buttons);
+        buttons.onclick = (function (ref) {
+            return function () {
+                goLink(ref);
+            };
+        })(ref);
+
+        buttons.innerText = name;
+        await nav.appendChild(buttons);
     }
+}
+
+function goLink(link) {
+    window.location.href = link;
 }
